72dd7f9f9789284e4f547fa65711ee38bb062a36
shubham sharma <shubhamtech004@gmail.com>
2024-12-27 02:46:53 +0530

    fetching backend api on login signup page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,17 +1,46 @@
 import React, { useState } from 'react'
 import login from '../Images/login.jpg'
 import logo from '../Images/Weblogo.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { api_base_url } from '../Helper.js'
 function LoginPage() {
     const[email,setEmail] = useState("")
     const[password,setPassword] = useState("")
+    const [error, setError] = useState("");
+
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    fetch(api_base_url + "/user/login",{
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    }).then(res => res.json()).then(data => {
+      if(data.success === true){
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("userId", data.userId)
+        setTimeout(() => {
+          window.location.href = "/"
+        }, 200);
+      } else {
+        setError(data.message);
+      }
+    })
+  }
   return (
     <>
     <h1 className='text-center mt-6 text-xl font-bold heading'>Welcome to The SHUBHCODING</h1>  
     <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
         <div className="left w-[35%] ">
         <img className='w-[200px] ms-16' src={logo} alt="" />
-        <form  className='w-full mt-[60px]' action="">
+        <form  className='w-full mt-[60px]' onClick={submitForm}>
         <div className="inputBox">
           <input required  onChange={(e)=>{setEmail(e.target.value)}} value={email} type="email" placeholder='Email'/>
         </div>
@@ -22,8 +51,7 @@ function LoginPage() {
 
         <p className='text-[gray]'>Don't have an account <Link to="/signup" className='text-[#00AEEF]'>Signup</Link></p>
 
-        <p className='text-red-500 text-[14px] my-2'></p>
-
+        <p className='text-red-500 text-[14px] my-2'>{error}</p>
         <button className="btnBlue w-full mt-[20px]">Login</button>
       </form>
         </div>
--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -1,20 +1,49 @@
 import React, { useState } from 'react'
 import signupImg from '../Images/Signupimg.jpg'
 import logo from'../Images/Weblogo.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { api_base_url } from '../Helper.js'
 
 function SignupPage() {
     const[username , setUserName] = useState("")
     const [name,setName] = useState("")
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const submitForm = (e) => {
+      e.preventDefault();
+      fetch(api_base_url + "/user/registaer",{
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          name: name,
+          email: email,
+          password: password
+        })
+      }).then((res)=>res.json()).then((data)=>{
+        if(data.success === true){
+          alert("Account created successfully");
+          navigate("/login"); 
+        }
+        else{
+          setError(data.message);
+        }
+      })
+    }
+
+  
   return (
     <>
         <h1 className='text-center mt-6 text-xl font-bold heading'>Welcome to The SHUBHCODING</h1>  
         <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
             <div className="left w-[35%] ">
             <img className='w-[200px] ms-16' src={logo} alt="" />
-            <form  className='w-full mt-[60px]' action="">
+            <form  onSubmit={submitForm} className='w-full mt-[60px]' >
             <div className="inputBox">
               <input required onChange={(e)=>{setUserName(e.target.value)}} value={username} type="text" placeholder='Username'/>
             </div>
@@ -33,7 +62,7 @@ function SignupPage() {
 
             <p className='text-[gray]'>Already have an account <Link to="/login" className='text-[#00AEEF]'>login</Link></p>
 
-            <p className='text-red-500 text-[14px] my-2'></p>
+            <p className='text-red-500 text-[14px] my-2'>{error}</p>
 
             <button className="btnBlue w-full mt-[20px]">Sign Up</button>
           </form>
